f73fc1c1a77c7313c4d93c0174066962a04657f0
Brandon <brandonkanute@gmail.com>
2022-03-31 20:21:17 +0300

    Fixed sections and populated content for all sections

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-let sum = (a, b)=> {
-    console.log(a+b)
+function validateForm (){
+    let name = document.getElementByID("name").value;
+    alert("Hello " + name + "I will get back to you as soon as possible")
 }
