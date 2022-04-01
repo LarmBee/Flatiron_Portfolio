2c86d34b9ff73196d4bd90d728cb094037fce5d3
Brandon <brandonkanute@gmail.com>
2022-04-01 12:58:36 +0300

    Made final chages .Pushing to github and pages

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function validateForm (){
-    let name = document.getElementByID("name").value;
+function validateForm(){
+    let name = document.getElementById("submit").value;
     alert("Hello " + name + "I will get back to you as soon as possible")
 }
+
+function downloadFile(elmnt) {
+    const link = elmnt
+    const url = 'https://assets.ctfassets.net/cfexf643femz/8rnHaKLBl6L4t1LmuV0OG/03d7ee1d08119fce2a3f80b93b97ab05/Lagos_de_Torca_en_cifras.pdf'
+  const options = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+    
+   fetch(url, options)
+    .then( response => {
+      response.blob().then(blob => {
+          let url = window.URL.createObjectURL(blob);
+          let a = document.createElement('a');
+          a.href = url;
+          a.download = "file.pdf";
+          a.click();
+        });
+      }); 
+  }
